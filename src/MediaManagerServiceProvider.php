12a4513fd338252a45897f935480a8e523a6ae04
Shane Perera <shane@shaneperera.com>
2015-10-31 22:14:31 +1100

    Initial Commit.

---
+++ src/MediaManagerServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace EMedia\MediaManager;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class MediaManagerServiceProvider extends ServiceProvider {
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->bind('emedia.mediaManager.fileHandler', function () {
+			return $this->app->make('EMedia\MediaManager\Files\FileHandler');
+		});
+
+		$this->app->bind('emedia.mediaManager.imageHandler', function () {
+			return $this->app->make('EMedia\MediaManager\Files\ImageHandler');
+		});
+	}
+}
